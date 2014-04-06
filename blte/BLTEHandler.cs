9948195d4d80c55139224ec2be620c9a4624c0a6
Michał Kałużny <michalkaluzny@gmail.com>
2014-04-06 11:30:30 +0200

    Next wave of findings

--- blte/BLTEHandler.cs
+++ blte/BLTEHandler.cs
@@ -1,79 +1,73 @@
 using System;
 using System.IO;
-
 using System.IO.Compression;
 using System.Security.Cryptography;
 
 namespace blte
 {
-    class BLTEChunk
-    {
-        public int compSize;
-        public int decompSize;
-        public byte[] hash;
-        public byte[] data;
-    }
+	class BLTEChunk
+	{
+		public int compSize;
+		public int decompSize;
+		public byte[] hash;
+		public byte[] data;
+	}
 
-    class BLTEHandler
-    {
-        BinaryReader reader;
-        string saveName;
-        MD5 md5 = MD5.Create();
+	class BLTEHandler
+	{
+		BinaryReader reader;
+		string saveName;
+		MD5 md5 = MD5.Create ();
 		long startPos = 0;
 
-        public string Name
-        {
-            get { return saveName; }
-        }
+		public string Name {
+			get { return saveName; }
+		}
 
-        public string Hash
-        {
-            get
-            {
-                using (var f = File.OpenRead(saveName))
-                {
-                    return md5.ComputeHash(f).ToHexString();
-                }
-            }
-        }
+		public string Hash {
+			get {
+				using (var f = File.OpenRead (saveName)) {
+					return md5.ComputeHash (f).ToHexString ();
+				}
+			}
+		}
 
-        public BLTEHandler(BinaryReader br)
-        {
+		public BLTEHandler (BinaryReader br)
+		{
 
-            reader = br;
+			reader = br;
 			startPos = reader.BaseStream.
 			           Position; // For debugging info;
-        }
+		}
 
-		public void ExtractData(string path, string name, int size)
-        {
-            int magic = reader.ReadInt32BE(); // BLTE (raw)
+		public void ExtractData (string path, string name, int size)
+		{
+			int magic = reader.ReadInt32BE (); // BLTE (raw)
 
-            if (magic != 0x424c5445)
-            {
-				Console.WriteLine ("Wrong Magic pos: {0}, start: {1} ",reader.BaseStream.Position, startPos);
+			if (magic != 0x424c5445) {
+				Console.WriteLine ("Wrong Magic pos: {0}, start: {1} ", reader.BaseStream.Position, startPos);
 				throw new Exception ();
 				return;
-            }
+			}
 
-			int compDataOffset = reader.ReadInt32BE();
+			int compDataOffset = reader.ReadInt32BE ();
 			int chunkCount = 0;
 
-			if(compDataOffset != 0) {
-				int unk1 = reader.ReadInt16BE();
-				chunkCount = reader.ReadInt16BE();
+			if (compDataOffset != 0) {
+				int unk1 = reader.ReadInt16BE ();
+				chunkCount = reader.ReadInt16BE ();
 			}
 
 			BLTEChunk[] chunks;
 
-			if (compDataOffset == 0) { //if this is 0 there's just one chunk with no chunk-header
+			if (compDataOffset == 0) { //if this is 0 there's just one chunk with no chunk-headers
 				chunkCount = 1;
 				chunks = new BLTEChunk[chunkCount];
 
 				chunks [0] = new BLTEChunk ();
 				chunks [0].compSize = size - 38; // no idea why.
 				chunks [0].hash = null;
-				chunks [0].decompSize = Int16.MaxValue;
+				chunks [0].decompSize = Int32.MaxValue;  // we need to find a way to properly handle these :/ No idea about the file size
 
 
 
@@ -96,89 +90,91 @@ namespace blte
 
 			string ext = "out";
 
-            if (!Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
+//            if (!Directory.Exists(path))
+//                Directory.CreateDirectory(path);
+//
 			string tmpName = path + "/" + name + ext;
-
 			var f = File.Open (tmpName, FileMode.Create);
 
-		    for (int i = 0; i < chunkCount; ++i)
-            {
+			for (int i = 0; i < chunkCount; ++i) {
 
 
-                chunks[i].data = reader.ReadBytes(chunks[i].compSize);
-				Console.WriteLine ("Now we are at {0}", reader.BaseStream.Position);
+				chunks [i].data = reader.ReadBytes (chunks [i].compSize);
 
 
-                byte[] hh = md5.ComputeHash(chunks[i].data);
+				byte[] hh = md5.ComputeHash (chunks [i].data);
 
-				if (compDataOffset != 0 && !hh.VerifyHash(chunks[i].hash))
-                {
-                    Console.WriteLine("MD5 missmatch!");
-                    return;
-                }
+				if (compDataOffset != 0 && !hh.VerifyHash (chunks [i].hash)) {
+					Console.WriteLine ("MD5 missmatch!");
+					return;
+				}
 
-                switch (chunks[i].data[0])
-                {
+				switch (chunks [i].data [0]) {
 				case 0x4E: // Not compressed;
-                        {
-						if ( (i == 0 || i == 0))
-							{
-								ext = GetFileExt(chunks[i].data, 1);
-							}
+					{
+						if ((i == 0 || i == 0)) {
+							ext = GetFileExt (chunks [i].data, 1);
+						}
 
-							if (chunks[i].data.Length - 1 != chunks[i].decompSize)
-                            {
-                                Console.WriteLine("Possible error (1) !");
-                            }
-						    f.Write(chunks[i].data, 1, chunks[i].decompSize);
-                        }
-                        break;
-                    case 0x5A:
-                        {
-							byte[] dec = Decompress(chunks[i].decompSize, chunks[i].data);
+						if (chunks [i].data.Length - 1 != chunks [i].decompSize) {
+							Console.WriteLine ("Possible error (1) !");
+						}
+						if (ext == "out")
+							f.Write (chunks [i].data, 1, chunks [i].decompSize);
+					}
+					break;
+				case 0x5A:
+					{
+						byte[] dec = Decompress (chunks [i].data);
 
-						if ((i == 0 || i == 0))
-							{
-							if (compDataOffset != 0)
-								ext = GetFileExt (dec, 0);
-							else
-								ext = "txt";
-							}
-
-							f.Write(dec, 0, dec.Length);
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Unknown byte {0:X2} at {1:X8}", chunks[i].data[0], reader.BaseStream.Position - chunks[i].data.Length);
-                        break;
-                }
-            }
+						if ((i == 0 || i == 0)) {
+							ext = GetFileExt (dec, 0);
+						}
+						if (ext == "out")
+							f.Write (dec, 0, dec.Length);
+					}
+					break;
+				default:
+					Console.WriteLine ("Unknown byte {0:X2} at {1:X8}", chunks [i].data [0], reader.BaseStream.Position - chunks [i].data.Length);
+					break;
+				}
+			}
 
 			string finalName = path + "/" + name + "." + ext;
 
-			f.Close ();
-			if(File.Exists(finalName))
-				File.Delete(finalName);
+			if (ext == "out") {
+				Console.WriteLine (finalName);
+				f.Close ();
+				if (File.Exists (finalName))
+					File.Delete (finalName);
+				File.Move (tmpName, finalName);
+			} else {
+				f.Close ();
+				if (File.Exists (tmpName))
+					File.Delete (tmpName);
+			}
+		}
 
-			File.Move(tmpName, finalName);
-        }
+		private byte[] Decompress (byte[] data)
+		{
+			using (var dStream = new DeflateStream (new MemoryStream (data, 3, data.Length - 3), CompressionMode.Decompress)) {
 
-        private byte[] Decompress(int size, byte[] data)
-        {
-			var output = new byte[size];
+				byte[] buffer = new byte[1024];
+				using (MemoryStream memory = new MemoryStream ()) {
+					int count = 0;
+					do {
+						count = dStream.Read (buffer, 0, 1024);
+						if (count > 0) {
+							memory.Write (buffer, 0, count);
+						}
+					} while (count > 0);
+					return memory.ToArray ();
+				}
+			}
+		}
 
-            using (var dStream = new DeflateStream(new MemoryStream(data, 3, data.Length - 3), CompressionMode.Decompress))
-            {
-                dStream.Read(output, 0, output.Length);
-            }
-
-            return output;
-        }
-
-        private string GetFileExt(byte[] data, int start)
-        {
+		private string GetFileExt (byte[] data, int start)
+		{
 			if (data.Length >= 18 && data [data.Length - 18] == 0x54 && data [data.Length - 17] == 0x52 && data [data.Length - 16] == 0x55 && data [data.Length - 15] == 0x45 && data [data.Length - 14] == 0x56)
 				return "tga";
 			else if (data [start + 0] == 0x49 && data [start + 1] == 0x44 && data [start + 2] == 0x33)
@@ -255,13 +251,31 @@ namespace blte
 				return "svxg";
 			else if (data [start + 0] == 0x42 && data [start + 1] == 0x4c && data [start + 2] == 0x50 && data [start + 3] == 0x32)
 				return "blp";
+			else if (data [start + 0] == 0x53 && data [start + 1] == 0x4b && data [start + 2] == 0x49 && data [start + 3] == 0x4e)
+				return "blp";
+			else if (data [start + 0] == 0x57 && data [start + 1] == 0x44 && data [start + 2] == 0x42 && data [start + 3] == 0x43)
+				return "wdbc";
+			else if (data [start + 0] == 0x52 && data [start + 1] == 0x45 && data [start + 2] == 0x56 && data [start + 3] == 0x4d)
+				return "skin"; // unknown file format
+			else if (data [start + 0] == 0x52 && data [start + 1] == 0x56 && data [start + 2] == 0x58 && data [start + 3] == 0x54)
+				return "rvxt"; // unknown file format
+			else if (data [start + 0] == 0x48 && data [start + 1] == 0x53 && data [start + 2] == 0x58 && data [start + 3] == 0x47)
+				return "hsxg"; // unknown file format
+			else if (data [start + 0] == 0x3c && data [start + 1] == 0x55 && data [start + 2] == 0x69)
+				return "xml"; // hack
+			else if (data [start + 0] == 0x4d && data [start + 1] == 0x44 && data [start + 2] == 0x32)
+				return "m2";
+			else if (data [start + 0] == 0x57 && data [start + 1] == 0x44 && data [start + 2] == 0x42 && data [start + 3] == 0x32)
+				return "wdb";
+			else if (data [start + 0] == 0x00 && data [start + 1] == 0x00 && data [start + 2] == 0x00)
+				return "unk"; // can not be guessed;
 			else {
 				byte[] header = new byte[3];
 				Array.Copy (data, start, header, 0, 3);
 
-				Console.WriteLine ("Unknown File: {0}", header.ToHexString());
+				Console.WriteLine ("Unknown File: {0}", header.ToHexString ());
 				return "out";
 			}
-        }
-    }
+		}
+	}
 }
