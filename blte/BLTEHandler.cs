835e7460b744b4582262b796f3aef8714a7d4a5f
Michał Kałużny <michalkaluzny@gmail.com>
2014-04-06 05:21:30 +0200

    Initial commit, very broken, much working

---
+++ blte/BLTEHandler.cs
@@ -0,0 +1,267 @@
+using System;
+using System.IO;
+
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace blte
+{
+    class BLTEChunk
+    {
+        public int compSize;
+        public int decompSize;
+        public byte[] hash;
+        public byte[] data;
+    }
+
+    class BLTEHandler
+    {
+        BinaryReader reader;
+        string saveName;
+        MD5 md5 = MD5.Create();
+		long startPos = 0;
+
+        public string Name
+        {
+            get { return saveName; }
+        }
+
+        public string Hash
+        {
+            get
+            {
+                using (var f = File.OpenRead(saveName))
+                {
+                    return md5.ComputeHash(f).ToHexString();
+                }
+            }
+        }
+
+        public BLTEHandler(BinaryReader br)
+        {
+
+            reader = br;
+			startPos = reader.BaseStream.
+			           Position; // For debugging info;
+        }
+
+		public void ExtractData(string path, string name, int size)
+        {
+            int magic = reader.ReadInt32BE(); // BLTE (raw)
+
+            if (magic != 0x424c5445)
+            {
+				Console.WriteLine ("Wrong Magic pos: {0}, start: {1} ",reader.BaseStream.Position, startPos);
+				throw new Exception ();
+				return;
+            }
+
+			int compDataOffset = reader.ReadInt32BE();
+			int chunkCount = 0;
+
+			if(compDataOffset != 0) {
+				int unk1 = reader.ReadInt16BE();
+				chunkCount = reader.ReadInt16BE();
+			}
+
+			BLTEChunk[] chunks;
+
+			if (compDataOffset == 0) { //if this is 0 there's just one chunk with no chunk-header
+				chunkCount = 1;
+				chunks = new BLTEChunk[chunkCount];
+
+				chunks [0] = new BLTEChunk ();
+				chunks [0].compSize = size - 38; // no idea why.
+				chunks [0].hash = null;
+				chunks [0].decompSize = Int16.MaxValue;
+
+
+
+			} else {
+
+				if (chunkCount < 0) {
+					Console.WriteLine ("No chunks found, compDataOffset: {0}, unk1: {1}, chunkCount: {2}, pos: {3}, start:  {4}", compDataOffset, false, chunkCount, reader.BaseStream.Position, startPos);
+					return;
+				}
+
+				chunks = new BLTEChunk[chunkCount];
+
+				for (int i = 0; i < chunkCount; ++i) {
+					chunks [i] = new BLTEChunk ();
+					chunks [i].compSize = reader.ReadInt32BE ();
+					chunks [i].decompSize = reader.ReadInt32BE ();
+					chunks [i].hash = reader.ReadBytes (16);
+				}
+			}
+
+			string ext = "out";
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+			string tmpName = path + "/" + name + ext;
+
+			var f = File.Open (tmpName, FileMode.Create);
+
+		    for (int i = 0; i < chunkCount; ++i)
+            {
+
+
+                chunks[i].data = reader.ReadBytes(chunks[i].compSize);
+				Console.WriteLine ("Now we are at {0}", reader.BaseStream.Position);
+
+
+                byte[] hh = md5.ComputeHash(chunks[i].data);
+
+				if (compDataOffset != 0 && !hh.VerifyHash(chunks[i].hash))
+                {
+                    Console.WriteLine("MD5 missmatch!");
+                    return;
+                }
+
+                switch (chunks[i].data[0])
+                {
+				case 0x4E: // Not compressed;
+                        {
+						if ( (i == 0 || i == 0))
+							{
+								ext = GetFileExt(chunks[i].data, 1);
+							}
+
+							if (chunks[i].data.Length - 1 != chunks[i].decompSize)
+                            {
+                                Console.WriteLine("Possible error (1) !");
+                            }
+						    f.Write(chunks[i].data, 1, chunks[i].decompSize);
+                        }
+                        break;
+                    case 0x5A:
+                        {
+							byte[] dec = Decompress(chunks[i].decompSize, chunks[i].data);
+
+						if ((i == 0 || i == 0))
+							{
+							if (compDataOffset != 0)
+								ext = GetFileExt (dec, 0);
+							else
+								ext = "txt";
+							}
+
+							f.Write(dec, 0, dec.Length);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Unknown byte {0:X2} at {1:X8}", chunks[i].data[0], reader.BaseStream.Position - chunks[i].data.Length);
+                        break;
+                }
+            }
+
+			string finalName = path + "/" + name + "." + ext;
+
+			f.Close ();
+			if(File.Exists(finalName))
+				File.Delete(finalName);
+
+			File.Move(tmpName, finalName);
+        }
+
+        private byte[] Decompress(int size, byte[] data)
+        {
+			var output = new byte[size];
+
+            using (var dStream = new DeflateStream(new MemoryStream(data, 3, data.Length - 3), CompressionMode.Decompress))
+            {
+                dStream.Read(output, 0, output.Length);
+            }
+
+            return output;
+        }
+
+        private string GetFileExt(byte[] data, int start)
+        {
+			if (data.Length >= 18 && data [data.Length - 18] == 0x54 && data [data.Length - 17] == 0x52 && data [data.Length - 16] == 0x55 && data [data.Length - 15] == 0x45 && data [data.Length - 14] == 0x56)
+				return "tga";
+			else if (data [start + 0] == 0x49 && data [start + 1] == 0x44 && data [start + 2] == 0x33)
+				return "mp3";
+			else if (data [start + 0] == 0x44 && data [start + 1] == 0x44 && data [start + 2] == 0x53)
+				return "dds";
+			else if (data [start + 0] == 0x4D && data [start + 1] == 0x5A && data [start + 2] == 0x90)
+				return "dll";
+			else if (data [start + 0] == 0x4D && data [start + 1] == 0x5A && data [start + 2] == 0x92)
+				return "dll";
+			else if (data [start + 0] == 0x43 && data [start + 1] == 0x46 && data [start + 2] == 0x58)
+				return "cfx";
+			else if (data [start + 0] == 0x4D && data [start + 1] == 0x50 && data [start + 2] == 0x51)
+				return "mpq";
+			else if (data [start + 0] == 0x43 && data [start + 1] == 0x57 && data [start + 2] == 0x53)
+				return "cws";
+			else if (data [start + 0] == 0x46 && data [start + 1] == 0x57 && data [start + 2] == 0x53)
+				return "fws";
+			else if (data [start + 0] == 0x3C && data [start + 1] == 0x3F && data [start + 2] == 0x78 && data [start + 3] == 0x6D && data [start + 4] == 0x6C)
+				return "xml";
+			else if (data [start + 0] == 0xEF && data [start + 1] == 0xBB && data [start + 2] == 0xBF && data [start + 3] == 0x3C && data [start + 4] == 0x3F && data [start + 5] == 0x78 && data [start + 6] == 0x6D && data [start + 7] == 0x6C)
+				return "xml";
+			else if (data [start + 0] == 0x62 && data [start + 1] == 0x70 && data [start + 2] == 0x6C && data [start + 3] == 0x69 && data [start + 4] == 0x73 && data [start + 5] == 0x74)
+				return "bplist";
+			else if (data [start + 0] == 0x46 && data [start + 1] == 0x41 && data [start + 2] == 0x43 && data [start + 3] == 0x45)
+				return "face";
+			else if (data [start + 0] == 0x63 && data [start + 1] == 0x64 && data [start + 2] == 0x65 && data [start + 3] == 0x73)
+				return "cdes";
+			else if (data [start + 0] == 0xCA && data [start + 1] == 0xFE && data [start + 2] == 0xBA && data [start + 3] == 0xBE)
+				return "o";
+			else if (data [start + 0] == 0xCE && data [start + 1] == 0xFA && data [start + 2] == 0xED && data [start + 3] == 0xFE)
+				return "o";
+			else if (data [start + 0] == 0x4F && data [start + 1] == 0x67 && data [start + 2] == 0x67 && data [start + 3] == 0x53)
+				return "ogg";
+			else if (data [start + 0] == 0x52 && data [start + 1] == 0x49 && data [start + 2] == 0x46 && data [start + 3] == 0x46)
+				return "wav";
+			else if (data [start + 0] == 0x89 && data [start + 1] == 0x50 && data [start + 2] == 0x4E && data [start + 3] == 0x47)
+				return "png";
+			else if (data [start + 0] == 0x52 && data [start + 1] == 0x54 && data [start + 2] == 0x58 && data [start + 3] == 0x54)
+				return "rtxt";
+			else if (data [start + 0] == 0x4C && data [start + 1] == 0x46 && data [start + 2] == 0x43 && data [start + 3] == 0x54)
+				return "lfct";
+			else if (data [start + 0] == 0x4D && data [start + 1] == 0x41 && data [start + 2] == 0x53 && data [start + 3] == 0x4B)
+				return "mask";
+			else if (data [start + 0] == 0x53 && data [start + 1] == 0x4D && data [start + 2] == 0x41 && data [start + 3] == 0x50)
+				return "smap";
+			else if (data [start + 0] == 0x48 && data [start + 1] == 0x4D && data [start + 2] == 0x41 && data [start + 3] == 0x50)
+				return "hmap";
+			else if (data [start + 0] == 0x43 && data [start + 1] == 0x4C && data [start + 2] == 0x49 && data [start + 3] == 0x46)
+				return "clif";
+			else if (data [start + 0] == 0x69 && data [start + 1] == 0x63 && data [start + 2] == 0x6E && data [start + 3] == 0x73)
+				return "icns";
+			else if (data [start + 0] == 0x6F && data [start + 1] == 0x72 && data [start + 2] == 0x65 && data [start + 3] == 0x48)
+				return "oreh";
+			else if (data [start + 0] == 0x57 && data [start + 1] == 0x41 && data [start + 2] == 0x54 && data [start + 3] == 0x52)
+				return "watr";
+			else if (data [start + 0] == 0x44 && data [start + 1] == 0x4C && data [start + 2] == 0x46 && data [start + 3] == 0x54)
+				return "dlft";
+			else if (data [start + 0] == 0x56 && data [start + 1] == 0x54 && data [start + 2] == 0x43 && data [start + 3] == 0x4C)
+				return "vtcl";
+			else if (data [start + 0] == 0x34 && data [start + 1] == 0x33 && data [start + 2] == 0x44 && data [start + 3] == 0x4D)
+				return "m3";
+			else if (data [start + 0] == 0x49 && data [start + 1] == 0x70 && data [start + 2] == 0x61 && data [start + 3] == 0x4D)
+				return "ipam";
+			else if (data [start + 0] == 0x48 && data [start + 1] == 0x52 && data [start + 2] == 0x44 && data [start + 3] == 0x54)
+				return "hrdt";
+			else if (data [start + 0] == 0x4D && data [start + 1] == 0x4E && data [start + 2] == 0x44 && data [start + 3] == 0x58)
+				return "mndx";
+			else if (data [start + 0] == 0x4F && data [start + 1] == 0x54 && data [start + 2] == 0x54 && data [start + 3] == 0x4F)
+				return "otf";
+			else if (data [start + 0] == 0x53 && data [start + 1] == 0x50 && data [start + 2] == 0x58 && data [start + 3] == 0x47)
+				return "spxg";
+			else if (data [start + 0] == 0x53 && data [start + 1] == 0x56 && data [start + 2] == 0x58 && data [start + 3] == 0x47)
+				return "svxg";
+			else if (data [start + 0] == 0x42 && data [start + 1] == 0x4c && data [start + 2] == 0x50 && data [start + 3] == 0x32)
+				return "blp";
+			else {
+				byte[] header = new byte[3];
+				Array.Copy (data, start, header, 0, 3);
+
+				Console.WriteLine ("Unknown File: {0}", header.ToHexString());
+				return "out";
+			}
+        }
+    }
+}
---
+++ blte/Program.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace blte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var file = File.OpenRead ("data.001");
+			var br = new BinaryReader (file, Encoding.ASCII);
+
+
+			while (br.BaseStream.Position != br.BaseStream.Length) {
+
+				long start = br.BaseStream.Position;
+
+				Console.WriteLine ("Parsing file at: {0}",start);
+
+				byte[] unkHash = br.ReadBytes(16);
+				int size = br.ReadInt32();
+				int unk1 = br.ReadInt32 ();
+				int unk2 = br.ReadInt16 ();
+				int unk3 = br.ReadInt32 ();
+
+				Console.WriteLine ("Size is {0} so next file is expected at {1}", size, start + size);
+
+				BLTEHandler h = new BLTEHandler(br);
+				h.ExtractData("out", unkHash.ToHexString(), size);
+			}
+		}
+	}
+
+	static class Extensions
+	{
+		public static int ReadInt32BE(this BinaryReader reader)
+		{
+			return BitConverter.ToInt32(reader.ReadBytes(4).Reverse().ToArray(), 0);
+		}
+
+		public static short ReadInt16BE(this BinaryReader reader)
+		{
+			return BitConverter.ToInt16(reader.ReadBytes(2).Reverse().ToArray(), 0);
+		}
+
+		public static string ToHexString(this byte[] data)
+		{
+			var str = String.Empty;
+			for (var i = 0; i < data.Length; ++i)
+				str += data[i].ToString("X2", CultureInfo.InvariantCulture);
+			return str;
+		}
+
+		public static bool VerifyHash(this byte[] hash, byte[] other)
+		{
+			for (var i = 0; i < hash.Length; ++i)
+			{
+				if (hash[i] != other[i])
+					return false;
+			}
+			return true;
+		}
+	}
+
+	public static class CStringExtensions
+	{
+		/// <summary> Reads the NULL terminated string from 
+		/// the current stream and advances the current position of the stream by string length + 1.
+		/// <seealso cref="BinaryReader.ReadString"/>
+		/// </summary>
+		public static string ReadCString(this BinaryReader reader)
+		{
+			return reader.ReadCString(Encoding.UTF8);
+		}
+
+		/// <summary> Reads the NULL terminated string from 
+		/// the current stream and advances the current position of the stream by string length + 1.
+		/// <seealso cref="BinaryReader.ReadString"/>
+		/// </summary>
+		public static string ReadCString(this BinaryReader reader, Encoding encoding)
+		{
+			try
+			{
+				var bytes = new List<byte>();
+				byte b;
+				while ((b = reader.ReadByte()) != 0)
+					bytes.Add(b);
+				return encoding.GetString(bytes.ToArray());
+			}
+			catch (EndOfStreamException)
+			{
+				return String.Empty;
+			}
+		}
+
+		public static void WriteCString(this BinaryWriter writer, string str)
+		{
+			var bytes = Encoding.UTF8.GetBytes(str);
+			writer.Write(bytes);
+			writer.Write((byte)0);
+		}
+	}
+}
